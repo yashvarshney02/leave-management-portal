1f4941f299add6cab442c36426e15182f5099089
yashvarshney02 <varshneyyash60@gmail.com>
2023-03-31 00:25:10 +0530

    Modified backend for users and leaves_data tables

--- server_new/app.py
+++ server_new/app.py
@@ -1,7 +1,6 @@
 from tkinter import E
 from flask import Flask, jsonify, render_template, url_for, request, session, redirect, Response
 from flask_pymongo import PyMongo
-# from matplotlib.pyplot import connect
 from py import code
 from pymongo import MongoClient
 from flask_mysqldb import MySQL
@@ -30,15 +29,43 @@ app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
 #     database="sql12599367"
 # )
 db = mysql.connector.connect(
-	host="localhost",
-	user="root",
-	passwd="root_user@dep2023",
-	database="yash_db"
+	host="yashiitrpr.mysql.pythonanywhere-services.com",
+	user="yashiitrpr",
+	passwd="password@dep123",
+	database="yashiitrpr$dep"
 )
+# db = mysql.connector.connect(
+# 	host="localhost",
+# 	user="root",
+# 	passwd="root_user@dep2023",
+# 	database="yash_db"
+# )
 print(db)
 
 success_code = Response(status=200)
 failure_code = Response(status=400)
+
+def get_default_number_of_leaves():
+	return {
+		"total_casual_leaves": 8,
+		"taken_casual_leaves": 0,
+		"total_restricted_leaves": 8,
+		"taken_restricted_leaves": 0,
+		"total_earned_leaves": 8,
+		"taken_earned_leaves": 0,
+		"total_vacation_leaves": 8,
+		"taken_vacation_leaves": 0,
+		"total_special_leaves": 8,
+		"taken_special_leaves": 0,
+		"total_commuted_leaves": 8,
+		"taken_commuted_leaves": 0,
+		"total_hospital_leaves": 8,
+		"taken_hospital_leaves": 0,
+		"total_study_leaves": 8,
+		"taken_study_leaves": 0,
+		"total_childcare_leaves": 8,
+		"taken_childcare_leaves": 0
+	}
 
 def get_error_response(error):
 	return {
@@ -63,48 +90,31 @@ def check_user(email):
 				return True
 		return False
 	except Exception as E:
-		return False   
+		return False
 
 def get_user_data(email):
 	try:
 		db.reconnect()
 		cursor = db.cursor()
-		cursor.execute("SELECT * FROM user where email_id=%s", (email, ))
+		cursor.execute("SELECT * FROM users where email_id=%s", (email, ))
 		data = cursor.fetchall()
 		return data
 	except Exception as E:
 		return {}
 
-def get_user_dic(email):	
+def get_user_dic(email):
 	data = get_user_data(email)[0]
-	dic = {}    
+	dic = {}
 	dic['user_id'] = data[0]
 	dic['name'] = data[1]
 	dic['email'] = data[2]
 	dic['position'] = data[3]
 	dic['department'] = data[4]
-	dic['total_casual_leaves'] = data[5]
-	dic['taken_casual_leaves'] = data[6]
-	dic['total_restricted_leaves'] = data[7]
-	dic['taken_restricted_leaves'] = data[8]
-	dic['total_earned_leaves'] = data[9]
-	dic['taken_earned_leaves'] = data[10]
-	dic['total_vacation_leaves'] = data[11]
-	dic['taken_vacation_leaves'] = data[12]
-	dic['total_special_leaves'] = data[13]
-	dic['taken_special_leaves'] = data[14]
-	dic['total_commuted_leaves'] = data[15]
-	dic['taken_commuted_leaves'] = data[16]
-	dic['total_hospital_leaves'] = data[17]
-	dic['taken_hospital_leaves'] = data[18]
-	dic['total_study_leaves'] = data[19]
-	dic['taken_study_leaves'] = data[20]
-	dic['total_childcare_leaves'] = data[21]
-	dic['taken_childcare_leaves'] = data[22]
+	dic['mobile'] = data[5]
 	return dic
 
 def insert_leave(leave):
-	try:		
+	try:
 		db.reconnect()
 		connect = db
 		cursor = connect.cursor()
@@ -117,11 +127,16 @@ def insert_leave(leave):
 			(department, user_id, nature, purpose, is_station, request_date, start_date, end_date, duration, status, level,file_uploaded) \
 			VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s)",
 					(department, user_id, leave['form_nature'], leave['form_purpose'], leave['form_isStation'], str(dt.date.today()), leave['form_sdate'], leave['form_edate'], leave['form_duration'], 'Pending', position, ''))
-		connect.commit()    
+		connect.commit()
 		return True
 	except Exception as E:
 		print(E)
 		return False
+
+@app.route('/home', methods = ['GET'])
+@cross_origin(supports_credentials=True)
+def home():
+	return get_success_response("Hello World")
 
 @app.route('/check_auth', methods = ['POST'])
 @cross_origin(supports_credentials=True)
@@ -132,23 +147,23 @@ def check_auth():
 		else:
 			return get_success_response(False)
 	except Exception as E:
-		return get_success_response(E)	
+		return get_success_response(E)
 
 @app.route('/send_otp', methods = ['POST'])
 @cross_origin(supports_credentials=True)
 def send_otp():
-	try:		
+	try:
 		session.clear()
 		email = request.json['email']
 		if (not check_user(email)):
 			return get_error_response("User not Allowed")
-		OTP = random.randint(10**5,10**6-1)		
-		session['otp'] = OTP		
+		OTP = random.randint(10**5,10**6-1)
+		session['otp'] = OTP
 		msg = "Your OTP for IIT Rpr Leave Management Portal is " + str(OTP)
 		s = smtplib.SMTP('smtp.gmail.com', 587)
 		s.starttls()
 		s.login("head.dep2023@gmail.com", "osgkkqldbkkinnqj")
-		s.sendmail('IIT Rpr Leave OTP',email,msg)		
+		s.sendmail('IIT Rpr Leave OTP',email,msg)
 		return get_success_response(f"OTP has been sent to {email}")
 	except Exception as E:
 		return get_success_response(E)
@@ -158,9 +173,9 @@ def send_otp():
 @cross_origin(supports_credentials=True)
 def validate_otp():
 	try:
-		otp = request.json['otp']  
-		email = request.json['email']  		
-		if str(otp) == str(session['otp']):        
+		otp = request.json['otp']
+		email = request.json['email']
+		if str(otp) == str(session['otp']):
 			session['logged_in'] = True
 			session['user_info'] = {
 				"email": email,
@@ -170,7 +185,7 @@ def validate_otp():
 			return get_success_response("OTP verified")
 		else:
 			session.clear()
-			return get_error_response("Wrong OTP")        
+			return get_error_response("Wrong OTP")
 	except Exception as E:
 		return get_error_response(E)
 
@@ -178,12 +193,14 @@ def validate_otp():
 @cross_origin(supports_credentials=True)
 def login_oauth():
 	try:
-		data = request.json['data'] 
-		session.clear()      
+		data = request.json['data']
+		if (not check_user(data.get('email'))):
+			return get_error_response("User not Allowed")
+		session.clear()
 		session['logged_in'] = True
 		session['user_info'] = data
 		session['user_info'].update(get_user_dic(email=data.get('email')))
-		return get_success_response("Details Saved")                       
+		return get_success_response("Details Saved")
 	except Exception as E:
 		return get_error_response(E)
 
@@ -191,7 +208,7 @@ def login_oauth():
 @cross_origin(supports_credentials=True)
 def logout():
 	try:
-		session.clear()                   
+		session.clear()
 		return get_success_response("Logout Successfull")
 	except Exception as E:
 		return get_error_response("Logout Failed")
@@ -200,10 +217,29 @@ def logout():
 @cross_origin(supports_credentials=True)
 def get_user_info():
 	try:
+		email = session['user_info']['email']
+		session['user_info'].update(get_user_dic(email=email))
 		return get_success_response(session.get('user_info'))
-	except Exception as E:		
+	except Exception as E:
 		return get_error_response(E)
-	
+
+@app.route('/edit_user_info' , methods = ['POST'])
+@cross_origin(supports_credentials=True)
+def edit_user_info():
+	try:
+		name = request.json['name']
+		mobile = request.json['mobile']
+		db.reconnect()
+		connect = db
+		cursor = connect.cursor()
+		email = session['user_info']['email']
+		query = 'UPDATE user set name = %s, mobile = %s WHERE email_id = %s'
+		cursor.execute(query,(name, mobile, email,))
+		connect.commit()
+		return get_success_response("Profile Edit successful")
+	except Exception as E:
+		return get_error_response(E)
+
 @app.route('/apply_leave', methods=['POST'])
 @cross_origin(supports_credentials=True)
 def apply_leave():
@@ -213,59 +249,47 @@ def apply_leave():
 		return get_success_response("Leave Applied Successfully")
 	except Exception as E:
 		return get_error_response("Leave Application Unsuccessful")
-	
+
 @app.route('/past_applications', methods=['POST'])
 @cross_origin(supports_credentials=True)
 def past_applications():
 	try:
-		email = session['user_info']['email']	
-		data = get_user_data(email)[0]	
-		user_id = data[0]	
+		email = session['user_info']['email']
+		data = get_user_data(email)[0]
+		user_id = data[0]
 		db.reconnect()
 		connect = db
 		cursor = connect.cursor()
 		cursor.execute("SELECT * FROM leaves WHERE user_id = %s", (user_id, ))
-		data = cursor.fetchall()	
+		data = cursor.fetchall()
 		payload = []
-		for i in data:			
+		for i in data:
 			content = {'id': i[0], 'department': i[1], 'user_id': i[2], 'nature': i[3], 'purpose': i[4], 'is_station': i[5], 'request_date': i[6],
 					'start_date': i[7], 'end_date': i[8], 'authority_comment': i[9], 'duration': i[10], 'status': i[11], 'level': i[12], 'attached_documents': i[13]}
-			user_id = i[2]        
+			user_id = i[2]
 			cur_user = get_user_dic(email)
 			content['email'] = cur_user['email']
-			content['name'] = cur_user['name']
-			nature = i[3]
-			c_st1 = "Total " + nature + 's'
-			c_st2 = "Taken " + nature + 's'
-			nature = nature.lower().split()
-			nature = '_'.join(nature)
-			u_st1 = 'total_' + nature + 's'
-			u_st2 = 'taken_' + nature + 's'
-
-			content[c_st1] = cur_user[u_st1]
-			content[c_st2] = cur_user[u_st2]
-			content["key1"] = c_st1
-			content["key2"] = c_st2
+			content['name'] = cur_user['name']			
 
 			payload.append(content)
-		connect.close()	
+		connect.close()
 
 		return get_success_response(payload)
 	except Exception as E:
 		return get_error_response(E)
-	
+
 @app.route('/check_applications', methods=['GET', 'POST'])
 @cross_origin(supports_credentials=True)
 def check_applications():
 	try:
-		email = session['user_info']['email']	
-		data = get_user_dic(email)	
+		email = session['user_info']['email']
+		data = get_user_dic(email)
 		user_id = data['user_id']
 		department = data['department']
 		position = data['position']
 		db.reconnect()
 		connect = db
-		cursor = connect.cursor()    
+		cursor = connect.cursor()
 		if position == "hod":
 			cursor.execute('SELECT * FROM leaves WHERE\
 				department = %s and level = %s', (department, "faculty"))
@@ -274,35 +298,24 @@ def check_applications():
 		elif position == 'establishment':
 			cursor.execute("SELECT * FROM leaves")
 		leaves = cursor.fetchall()
-		payload = []	
-		for i in leaves:		
+		payload = []
+		for i in leaves:
 			content = {'id': i[0], 'department': i[1], 'user_id': i[2], 'nature': i[3], 'purpose': i[4], 'is_station': i[5], 'request_date': i[6],
-					'start_date': i[7], 'end_date': i[8], 'authority_comment': i[9], 'duration': i[10], 'status': i[11], 'level': i[12], 'attached_documents': i[13]}        
+					'start_date': i[7], 'end_date': i[8], 'authority_comment': i[9], 'duration': i[10], 'status': i[11], 'level': i[12], 'attached_documents': i[13]}
 			user_id = i[2]
 			db.reconnect()
 			connect = db
 			cursor = connect.cursor()
 			cursor.execute(
 				'SELECT email_id FROM user WHERE user_id = %s', (user_id, ))
-			data = cursor.fetchall()		
+			data = cursor.fetchall()
 			email = data[0][0]
-			cur_user = get_user_dic(email)		
+			cur_user = get_user_dic(email)
 			content['email'] = cur_user['email']
 			content['name'] = cur_user['name']
 			nature = i[3]
-			c_st1 = "Total " + nature + 's'
-			c_st2 = "Taken " + nature + 's'
-			nature = nature.lower().split()
-			nature = '_'.join(nature)
-			u_st1 = 'total_' + nature + 's'
-			u_st2 = 'taken_' + nature + 's'
-
-			content[c_st1] = cur_user[u_st1]
-			content[c_st2] = cur_user[u_st2]
-			content["key1"] = c_st1
-			content["key2"] = c_st2        
-			if position == 'dean':            
-				if (nature == "casual_leave" or nature == "restricted_leave") and (content['status'] == 'Pending'):
+			if position == 'dean':
+				if (nature == "casual_leave" or nature == "restricted_leave"):
 					payload.append(content)
 				elif nature == "casual_leave" or nature == "restricted_leave":
 					continue
@@ -310,21 +323,51 @@ def check_applications():
 					payload.append(content)
 			elif position == 'hod':
 				payload.append(content)
-			elif position == 'establishment':
-				if content['status'] == 'Approved By Hod':
-					payload.append(content)		
 		return get_success_response(payload)
 	except Exception as E:
-		return get_error_response(E)	
+		return get_error_response(E)
 	
+@app.route('/fetch_remaining_leaves', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def fetch_remaining_leaves():
+	try:
+		email = session['user_info']['email']
+		data = get_user_data(email)[0]
+		user_id = data[0]
+		year = dt.date.today().year
+		connect = db
+		cursor = connect.cursor()		
+		cursor.execute("SELECT * FROM leaves_data WHERE user_id=%s and year=%s", (user_id, year))
+		data = cursor.fetchall()
+		default_leaves = get_default_number_of_leaves()			
+		columns = ["user_id"]
+		for e in default_leaves:
+			columns.append(e)
+		columns.append("year")
+		if len(data) == 0:			
+			values = [user_id]
+			for e in default_leaves:
+				values.append(default_leaves[e])			
+			values.append(year)
+			query = "INSERT INTO leaves_data ({}) VALUES ({})".format(
+    		', '.join(columns), ', '.join(['%s'] * len(values)))
+			cursor.execute(query, tuple(values))
+			connect.commit()
+			return get_success_response(default_leaves)
+		else:
+			payload = {key: val for key,val in zip(columns,data[0])}
+			return get_success_response(payload)
+	except Exception as E:
+		return get_error_response(E)
+
 @app.route('/fetch_number_of_leaves', methods=['POST'])
 @cross_origin(supports_credentials=True)
-def fetchNumberOfLeaves():
+def fetch_number_of_leaves():
 	try:
 		email = session['user_info']['email']
 		data = get_user_data(email)[0]
 		if data[3] !='dean':
-			return jsonify(result=[])		
+			return get_success_response([])
 		db.reconnect()
 		connect = db
 		cursor = connect.cursor()
@@ -333,15 +376,171 @@ def fetchNumberOfLeaves():
 		pprint.pprint(data)
 		payload = []
 		for i in data:
-			if (i[3] == 'hod'):            
-				content = [i[2], i[1], i[3], i[4], i[5] - i[6]]        
+			if (i[3] == 'hod'):
+				content = [i[2], i[1], i[3], i[4], i[5] - i[6]]
 				payload.append(content)
 		return get_success_response(payload)
 	except Exception as E:
-		return get_error_response(E)    
+		return get_error_response(E)
 
-		
+@app.route('/approve_leave', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def approve_leave():
+	try:
+		leave_id = request.json['leave_id']
+		if "approved" not in session:
+			session['approved'] = {}
+		if leave_id in session['approved']:
+			return get_success_response(f"Leave with ID: {leave_id} is approved")
+		session['approved'][leave_id] = 1
+		user = get_user_dic(session['user_info']['email'])
+
+		db.reconnect()
+		connect = db
+		cursor = connect.cursor()
+		if user["position"] == "hod":
+			cursor.execute(
+				"UPDATE leaves SET status = 'Approved By Hod' WHERE leave_id = %s", (leave_id, ))
+		elif user["position"] == "dean":
+			cursor.execute(
+				"UPDATE leaves SET status = 'Approved By Dean' WHERE leave_id = %s", (leave_id, ))
+		connect.commit()
+
+		cursor.execute(
+			"Select user_id, nature, duration from leaves where leave_id = %s", (leave_id, ))
+		data = cursor.fetchall()[0]
+		user_id = data[0]
+		nature = data[1]
+		duration = float(data[2])
+
+		nature = nature.lower().split()
+		nature = '_'.join(nature)
+		u_st1 = 'total_' + nature + 's'
+		u_st2 = 'taken_' + nature + 's'
+		query = "Select %s from user where user_id = %s" % (u_st2, user_id)
+		cursor.execute(query)
+		data = cursor.fetchall()[0]
+		taken_cnt = float(data[0]) + duration
+		if (nature == "casual_leave" or nature == "restricted_leave") and (user['position'] == 'hod' or user['position'] == 'dean'):
+			query = "Update user set %s = %s where user_id = %s" % (
+				u_st2, taken_cnt, user_id)
+			cursor.execute(query)
+		elif nature != "casual_leave" and nature != "restricted_leave" and user['position'] == 'dean':
+			query = "Update user set %s = %s where user_id = %s" % (
+				u_st2, taken_cnt, user_id)
+			cursor.execute(query)
+		connect.commit()
+		connect.close()
+		# send_update_mail(leave_id)
+		return get_success_response(f"Leave with ID: {leave_id} is approved")
+	except Exception as E:
+		return get_error_response(E)
+
+@app.route('/disapprove_leave', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def disapprove_leave():
+	try:
+		leave_id = request.json['leave_id']
+		user = get_user_dic(session['user_info']['email'])
+		db.reconnect()
+		connect = db
+		cursor = connect.cursor()
+		if user["position"] == "hod":
+			cursor.execute(
+				"UPDATE leaves SET status = 'Disapproved By Hod' WHERE leave_id = %s", (leave_id,))
+		elif user["position"] == "dean":
+			cursor.execute(
+				"UPDATE leaves SET status = 'Disapproved By Hod' WHERE leave_id = %s", (leave_id,))
+		connect.commit()
+		connect.close()
+		return get_success_response(f"Leave with ID: {leave_id} is disapproved")
+	except Exception as E:
+		return get_error_response(E)
+
+@app.route('/add_comment', methods=['POST'])
+def add_comment():
+	try:
+		leave_id = request.json['leave_id']
+		comment = request.json['comment']
+		db.reconnect()
+		connect = db
+		cursor = connect.cursor()
+		cursor.execute(
+			"UPDATE leaves SET authority_comment = %s WHERE leave_id = %s", (comment, leave_id))
+		connect.commit()
+		connect.close()
+		return get_success_response(f"Your comment is added to Leave with ID : {leave_id}")
+	except Exception as E:
+		return get_error_response(E)
+
+@app.route('/delete_application', methods=['POST'])
+def delete_application():
+	try:
+		leave_id = request.json['leave_id']
+		db.reconnect()
+		connect = db
+		cursor = connect.cursor()
+		cursor.execute("SELECT status FROM leaves where leave_id = %s", (leave_id,))
+		results = cursor.fetchall()
+		status = results[0][0]
+		if ("Approved" in status):
+			return get_error_response("This leave has already been approved, can't be deleted")
+		if ("Disapproved" in status):
+			return get_error_response("This leave has already been disapproved, can't be deleted")
+		cursor.execute("DELETE FROM leaves where leave_id = %s", (leave_id,))
+		connect.commit()
+		connect.close()
+		return get_success_response(f"Leave with id : {leave_id} has been deleted, refresh the page to see changes")
+	except Exception as E:
+		return get_error_response(E)
+
+@app.route('/get_holidays_info', methods=['GET'])
+def get_holiday_info():
+	try:
+		curr_year = dt.datetime.now().year
+		db.reconnect()
+		connect = db
+		cursor = connect.cursor()
+		cursor.execute(f"CREATE TABLE IF NOT EXISTS holidays_{curr_year} (date VARCHAR(50) UNIQUE)")
+		connect.commit()
+		query = "SHOW TABLES LIKE 'holidays%'"
+		cursor.execute(query)
+		results = cursor.fetchall()
+		tables = [table[0] for table in results]
+		payload = {}
+		for table in tables:
+			year = table.split("_")[1]
+			holidays = []
+			payload[year] = []
+			cursor.execute(f"SELECT * FROM {table}")
+			results = cursor.fetchall()
+			holidays = [res[0] for res in results]
+			payload[year] = holidays
+		connect.close()
+		return get_success_response(payload)
+	except Exception as E:
+		return get_error_response(E)
+
+@app.route('/add_holiday', methods=['POST'])
+def add_holiday():
+	try:
+		curr_year = request.json['year']
+		new_holidays = request.json['holidays']
+		db.reconnect()
+		connect = db
+		cursor = connect.cursor()
+		cursor.execute(f"TRUNCATE TABLE holidays_{curr_year}")
+		connect.commit()
+		for date in new_holidays:
+			query = f"INSERT INTO holidays_{curr_year}(date) values('{date[0]}')"
+			cursor.execute(query)
+		connect.commit()
+		connect.close()
+		return get_success_response("Holidays Added successfully")
+	except Exception as E:
+		return get_error_response(E)
+
 
 if __name__ == '__main__':
-	app.secret_key='secret123'    
-	app.run(debug=True)
+	app.secret_key='secret123'
+	app.run(host='0.0.0.0: 3001')
